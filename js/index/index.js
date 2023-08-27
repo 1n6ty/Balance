7332254d67e3db75de9cea698619dd7bf2e7fcba
1n6ty <artyom.inety@gmail.com>
2023-08-28 03:48:39 +0700

    done what_need section

--- js/index/index.js
+++ js/index/index.js
@@ -4,3 +4,15 @@ var top_nav_btn = document.querySelector('nav.top_nav .nav_btn'),
 top_nav_btn.addEventListener('click', (e) => {
     top_nav.classList.toggle('active');
 }, false);
+
+$(document).ready(function(){
+    $('.swiper').slick({
+        arrows: true,
+        autoplay: true,
+        prevArrow: $('.btn-prev'),
+        nextArrow: $('.btn-next'),
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+});
